c0ed105a08f3fcb37d5b95420ef6847f4241b32b
ceptar <135349980+ceptar@users.noreply.github.com>
2023-10-31 09:30:30 +0100

    Update sessions.ts

--- app/sessions.ts
+++ app/sessions.ts
@@ -1,20 +1,5 @@
-import { IS_CF_PAGES } from '~/utils/platform-adapter';
 import { SessionStorage } from '@remix-run/server-runtime/dist/sessions';
 import { ErrorResult } from '~/generated/graphql';
-
-function getCookieSessionStorageFactory() {
-  if (IS_CF_PAGES) {
-    return require('@remix-run/cloudflare').createCookieSessionStorage;
-  } else {
-    // This hack is to prevent the `node` package being bundled in the
-    // Cloudflare Pages context, which causes an error.
-    let imp = ['@remix-run', 'node'];
-    return require(imp.join('/')).createCookieSessionStorage;
-  }
-}
-let sessionStorage: SessionStorage<
-  { activeOrderError: ErrorResult } & Record<string, any>
->;
 
 export function getSessionStorage() {
   if (sessionStorage) {
