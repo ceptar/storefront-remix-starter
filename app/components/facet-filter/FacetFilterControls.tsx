b6dcd6d9a0d3278b530da2d2dea0198873be3ea8
Christoph Cerjan <christoph.cerjan@gmail.com>
2023-12-08 22:57:08 +0100

    cc

--- app/components/facet-filter/FacetFilterControls.tsx
+++ app/components/facet-filter/FacetFilterControls.tsx
@@ -73,7 +73,7 @@ export default function FacetFilterControls({
                         <>
                           <h3 className="-mx-2 -my-3 flow-root">
                             <Disclosure.Button className="px-2 py-3 bg-white w-full flex items-center justify-between text-gray-400 hover:text-gray-500">
-                              <span className="font-medium text-gray-900 uppercase">
+                              <span className="font-medium text-gray-900 tracking-[0.15em] uppercase">
                                 {facet.name}
                               </span>
                               <span className="ml-6 flex items-center">
@@ -147,7 +147,7 @@ export default function FacetFilterControls({
               <>
                 <h3 className="-my-3 flow-root">
                   <Disclosure.Button className="py-3 bg-white w-full flex items-center justify-between text-sm text-gray-400 hover:text-gray-500">
-                    <span className="font-medium text-gray-900 uppercase">
+                    <span className="font-medium text-gray-900 tracking-[0.15em]  uppercase">
                       {facet.name}
                     </span>
                     <span className="ml-6 flex items-center">
--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -38,7 +38,7 @@ export function Header({
         <div className="relative px-4 flex flex-row items-center justify-between h-[8vh] w-full ">
           <div className="relative  flex flex-col  items-start justify-start w-1/6">
             <button
-              className="flex flex-col  bg-opacity-90 shadow-none cursor-pointer justify-center rounded-full items-center p-2 text-sm text-discogray-500 transition-all duration-300 ease-out hover:opacity-70"
+              className="flex flex-col  bg-opacity-90 shadow-none cursor-pointer justify-center rounded-full items-center p-2 text-sm text-discogray transition-all duration-300 ease-out hover:opacity-70"
               onClick={onCartIconClick}
               aria-label="Open cart tray"
             >
